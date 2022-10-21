de159aae5be13005135a8226429c41a124893ec7
Max Karpov <maxusmax2@gmail.com>
2022-10-21 16:17:27 +0300

    Добавлена библиотека для АЦП
    Считывание и обработка АЦП. Бибилотека в которой содержится вся логика проекта

---
+++ Core/Inc/sensor_manager.h
@@ -0,0 +1,26 @@
+/*
+ * sensor_manager.h
+ *
+ *  Created on: 17 окт. 2022 г.
+ *      Author: UUG009
+ */
+#include "adc.h"
+#include "gpio.h"
+#define COUNT_SCAN_ADC 10
+#define ADC_CHANNELS_NUM 3
+#define ADC_RESOLUTION 4096
+#define VREFINT_CAL 1645.00
+#define VREF 3.00
+
+#define SENSOR1 0
+#define SENSOR2 1
+#define SENSOR3 2
+#define VREFINT 3
+typedef struct{
+	float result_sensor_1;
+	float result_sensor_2;
+	float result_sensor_3;
+}result_sensor;
+void show_result(result_sensor * result);
+void get_result_sensor(result_sensor * result);
+float get_charge(uint16_t result,uint16_t standart_voltage);
---
+++ Core/Src/sensor_manager.c
@@ -0,0 +1,70 @@
+/*
+ * sensor_manager.c
+ *
+ *  Created on: 17 окт. 2022 г.
+ *      Author: UUG009
+ */
+#include "sensor_manager.h"
+
+uint8_t dma_flag = 0;
+extern uint16_t adc_data[ADC_CHANNELS_NUM];
+void get_result_sensor(result_sensor * result)
+{
+	result->result_sensor_1 = result->result_sensor_2 = result->result_sensor_3 = 0;
+	for(int  i = 0; i < COUNT_SCAN_ADC; i++)
+	{
+	  wait_dma();
+	  result->result_sensor_1 += adc_data[SENSOR1];//get_charge(adc_data[SENSOR1], adc_data[VREFINT] );
+	  result->result_sensor_2 += adc_data[SENSOR2];//get_charge(adc_data[SENSOR2], adc_data[VREFINT] );
+	  result->result_sensor_3 += adc_data[SENSOR3];//get_charge(adc_data[SENSOR3], adc_data[VREFINT] );
+	  dma_flag = 0;
+	}
+
+	result->result_sensor_1 /= COUNT_SCAN_ADC;
+	result->result_sensor_2 /= COUNT_SCAN_ADC;
+	result->result_sensor_3 /= COUNT_SCAN_ADC;
+
+}
+
+
+void wait_dma()
+{
+	while(1)
+	{
+		if(dma_flag)
+		{
+			break;
+		}
+	}
+}
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
+{
+	dma_flag = 1;
+}
+
+float get_charge(uint16_t result,uint16_t standart_voltage)
+{
+	return (VREF * result * standart_voltage ) / (ADC_RESOLUTION * VREFINT_CAL);
+}
+
+void show_result(result_sensor * result)
+{
+	HAL_GPIO_WritePin(GPIOA,GPIO_PIN_15,GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOA,GPIO_PIN_12,GPIO_PIN_RESET);
+
+	if(result->result_sensor_1 > 3500)
+	{
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_12,GPIO_PIN_SET);
+	}
+	if(result->result_sensor_2 > 3500)
+	{
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_15,GPIO_PIN_SET);
+	}
+	if(result->result_sensor_3 > 3500)
+	{
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_12,GPIO_PIN_SET);
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_15,GPIO_PIN_SET);
+	}
+
+}
